033de3e51bc7a2a18d0e8c3c6933e9b3379aed46
KouT127 <kou26haru@gmail.com>
2020-06-10 00:54:31 +0900

    modify: add access logger

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+app.use(morgan('dev'));
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ message: 'ok' });
+});
 
 app.get('/', (_req, res) => {
     res.send(`<h1>TypeScript Express!</h1>`);
